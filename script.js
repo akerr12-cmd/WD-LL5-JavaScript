f9987d4294abac4ce9258914a5bdbde157caaf16
Amanda Kerr <akerr12@asu.edu>
2026-02-11 02:05:46 +0000

    JS training

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("Hello, World");
+console.error("This is my first JavaScript error message!");
+alert("Hi Adam");
+alert("Hi Jeremy");
+alert("Hi Aryan");
+alert("Hi Pranav");
+console.log(console);
+
+alert("Welcome to my site!"); alert("Thanks for visitng");
